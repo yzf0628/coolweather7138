86dbc77e90d544615189a9ef7755d313d2545fb9
yzf0628 <2295023441@qq.com>
2024-05-28 15:17:46 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -1,6 +1,8 @@
 package com.coolweather.android.util;
 import android.text.TextUtils;
 
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
 
 import org.json.JSONArray;
@@ -19,6 +21,45 @@ public class Utility {
                     province.setProvinceName(jsonObject.getString("name"));
                     province.save();
                 }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    public static boolean handleCityResp(String jsonStr,int provinceId){
+        if (!TextUtils.isEmpty(jsonStr)){
+            try {
+                JSONArray array=new JSONArray(jsonStr);
+                for (int i = 0; i<array.length(); i++){
+                    JSONObject jsonObject = array.getJSONObject(1);
+                    City city =new City();
+                    city.setCityCode(jsonObject.getInt("id"));
+                    city.setCityName(jsonObject.getString("name"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    public static boolean handleCountyResp(String jsonStr,int cityId){
+        if (!TextUtils.isEmpty(jsonStr)){
+            try {
+                JSONArray array=new JSONArray(jsonStr);
+                for (int i = 0; i<array.length(); i++){
+                    JSONObject jsonObject = array.getJSONObject(1);
+                    County county=new County();
+                    county.setCountyName(jsonObject.getString("name"));
+                    county.setCityId(cityId);
+                    county.setWeatherId(jsonObject.getString("weather_id"));
+                    county.save();
+                }
+                return true;
             } catch (JSONException e) {
                 e.printStackTrace();
             }
